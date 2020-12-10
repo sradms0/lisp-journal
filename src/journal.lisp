fed2970aec32e315b63b7cda97ac3929d8f60046
Sean Blackford <57670508+SeanBlackford37@users.noreply.github.com>
2020-12-09 22:22:45 -0500

    Updated
    get-all-entries works, (Search for entry does not)

--- src/journal.lisp
+++ src/journal.lisp
@@ -2,7 +2,9 @@
   (:use :cl)
   (:export :journal
            :add-entry
-           :entries))
+           :entries
+           :get-all-entries
+           :search-for-entry))
 (in-package :journal-package)
   
 (defclass journal ()
@@ -17,3 +19,20 @@
 (defmethod add-entry ((object journal) entry)
   "Add a new entry to this journal"
     (setf (entries object) (append (entries object) (list entry))))
+
+(defmethod get-all-entries ((object journal))
+  "Get all the entries"
+   (return-from get-all-entries (entries object))
+)
+
+
+(defmethod search-for-entry ((object journal) text)
+  "Search for an entry in the journal"
+  (loop for x from 0 to  (- (list-length (entries object)) 1)
+  if(string= (let* ((journal-entry (nth x (entries object)))) (journal-entry title)) text)
+  do(return-from search-for-entry (journal-entry)))
+  )
+
+
+
+
--- src/main.lisp
+++ src/main.lisp
@@ -22,23 +22,14 @@
       (entry-package:text journal-entry) 
       (entry-package:bookmark journal-entry))))
 
+(defun get-all-entries-journal ()
+   "Get all the entries of journal"
+   (journal-package:get-all-entries *journal-1*))
 
+(defun search-for-entry-journal (text)
+   "Get all the entries of journal"
+   (journal-package:search-for-entry *journal-1* text))
 
-
-(defun show-all-entry ()
-   "Show all the entries of journal"
-  (loop for x from 0 to  (- (list-length (journal-package:entries *journal-1*)) 1)
-  do (print(show-nth-entry x)))
-)
-
-(defun search-for-entry (text)
-  "Search for an entry in the journal"
-  
-  (loop for x from 0 to  (- (list-length (journal-package:entries *journal-1*)) 1)
-  if(string= (let* ((journal-entry (nth x (journal-package:entries *journal-1*)))) (entry-package:title journal-entry)) text)
-  do(print(show-nth-entry x))
-)
-)
 
 
 (add-entry-to-journal "entry title 1" "entry text 1")
@@ -47,17 +38,9 @@
 (show-nth-entry 1)
 (add-entry-to-journal "entry title 3" "entry text 3")
 
-(show-all-entry)
-(search-for-entry "entry title 1")
 
+;;(get-all-entries-journal)
+(search-for-entry-journal "entry title 1")
 
-;; Length of the journal list 
-;; (print (list-length (journal-package:entries *journal-1*)))
-
-;;Displays first entry only the title
-;;(print(let* ((journal-entry (nth 0 (journal-package:entries *journal-1*)))) (entry-package:title journal-entry)))
-
-;;Comparing the first entry in the journal to a string
-;;(print(string= (let* ((journal-entry (nth 0 (journal-package:entries *journal-1*)))) (entry-package:title journal-entry)) "entry title 1"))
       
 
