28e335426301d341bfb653a8d66043bf9d07383d
Eli Robinson <elimrobinson@gmail.com>
2020-12-15 18:05:37 -0500

    Update userInterface.lisp

--- src/userInterface.lisp
+++ src/userInterface.lisp
@@ -12,6 +12,10 @@
   (read-line *query-io*))
 
 
+;Needs a top layer 
+;asks user what function to run
+;runs selected function
+
 (defun add-entry-to-journal (title text)
   "Creates and adds a new entry to journal"
   (journal-package:add-entry *journal-1* (make-instance 'entry 
@@ -22,6 +26,17 @@
   (add-entry-to-journal
     (prompt-read "Enter Title")
     (prompt-read "Enter text")))
+
+(defun search-for-entry-user-input()
+  (search-for-entry
+    (prompt-read "Enter Title to find")))
+
+(defun remove-entry-user-input()
+  (remove-entry
+    (prompt-read "Enter Title to remove")))    
+
+
+
   
 (defun show-nth-entry (n)
   "Show the nth entry of journal"
